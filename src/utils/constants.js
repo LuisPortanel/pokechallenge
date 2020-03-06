4c6703a6aedaa8fa362f32c40c7b7e4d38ac1b1a
Luis Portanel <luis@portanel.com>
2020-03-06 09:36:50 -0600

    PKMN: Create constants file.

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+// Useful constants
+export const API = 'https://pokeapi.co/api/v2/'
+
+// Actions
+export const SAVE_LIST_POKEMON = 'SAVE_LIST_POKEMON'
+export const UPDATE_LOAD_MORE_URL = 'UPDATE_LOAD_MORE_URL'

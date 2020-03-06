97793640b8a15f9cd619d6af644f8b52cb4d2196
Luis Portanel <luis@portanel.com>
2020-03-06 09:02:30 -0600

    PKMN: Implement store on index.js with devtools and redux-thunk. Apply on App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './assets/App.css'
+import { Provider } from 'react-redux'
 
-function App () {
-  return (
+const App = ({ store }) => (
+
+  <Provider store={store}>
     <div className="App">
       <h1>Pokemon Challenge</h1>
     </div>
-  )
-}
+  </Provider>
+)
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import './assets/index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './redux/reducers'
+import ReduxThunk from 'redux-thunk'
+
+const store = createStore(
+  rootReducer,
+  // initial State,
+  composeWithDevTools(applyMiddleware(ReduxThunk))
+)
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
